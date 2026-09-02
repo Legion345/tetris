7dd7bdea2b7f467d4c3b6e29412b809266c979ae
Legion345 <harelasaraf@gmail.com>
2026-09-02 15:30:31 -0700

    Add game loop, score display, boss mode and save-on-exit

--- src/main.c
+++ src/main.c
@@ -78,3 +78,223 @@ void display_piece(WINDOW *w, tetris_block block) {
   }
   wnoutrefresh(w);
 }
+
+// Display score information in a dedicated window
+void display_score(WINDOW *w, tetris_game *tg) {
+  wclear(w);
+  box(w, 0, 0);
+  wprintw(w, "Score\n%d\n", tg->points);
+  wprintw(w, "Level\n%d\n", tg->level);
+  wprintw(w, "Lines\n%d\n", tg->lines_remaining);
+  wnoutrefresh(w);
+}
+
+// Boss mode! Make it look like you're doing work
+void boss_mode(void) {
+  clear();
+#if WITH_SDL
+  Mix_PauseMusic();
+#endif
+  printw("user@workstation-312:~/Documents/presentation $ ls -l\n"
+         "total 528\n"
+         "drwxr-xr-x 2 user users   4096 Jun  9 17:05 .\n"
+         "drwxr-xr-x 4 user users   4096 Jun 10 09:52 ..\n"
+         "-rw-r--r-- 1 user users  88583 Jun  9 14:13 figure1.png\n"
+         "-rw-r--r-- 1 user users  65357 Jun  9 15:40 figure2.png\n"
+         "-rw-r--r-- 1 user users   4469 Jun  9 16:17 presentation.aux\n"
+         "-rw-r--r-- 1 user users  42858 Jun  9 16:17 presentation.log\n"
+         "-rw-r--r-- 1 user users   2516 Jun  9 16:17 presentation.nav\n"
+         "-rw-r--r-- 1 user users    183 Jun  9 16:17 presentation.out\n"
+         "-rw-r--r-- 1 user users 349607 Jun  9 16:17 presentation.pdf\n"
+         "-rw-r--r-- 1 user users      0 Jun  9 16:17 presentation.snm\n"
+         "-rw-r--r-- 1 user users   9284 Jun  9 17:05 presentation.tex\n"
+         "-rw-r--r-- 1 user users    229 Jun  9 16:17 presentation.toc\n"
+         "\n"
+         "user@workstation-312:~/Documents/presentation $ ");
+  echo();
+  timeout(-1);
+  while (getch() != KEY_F(1))
+    ;
+  timeout(0);
+  noecho();
+  clear();
+#if WITH_SDL
+  Mix_ResumeMusic();
+#endif
+}
+
+// Save and exit the game
+
+void save(tetris_game *game, WINDOW *w) {
+  FILE *f;
+
+  wclear(w);
+  box(w, 0, 0); // return the border
+  wmove(w, 1, 1);
+  wprintw(w, "Save and exit? [Y/n] ");
+  wrefresh(w);
+  timeout(-1);
+  if (getch() == 'n') {
+    timeout(0);
+    return;
+  }
+  f = fopen("tetris.save", "w");
+  tg_save(game, f);
+  fclose(f);
+  tg_delete(game);
+  endwin();
+  printf("Game saved to \"tetris.save\".\n");
+  printf("Resume by passing the filename as an argumet to this program.\n");
+  exit(EXIT_SUCCESS);
+}
+
+// Do the NCURSES initialization steps for color blocks
+void init_colors(void) {
+  start_color();
+  // init_color(COLOR_ORANGE, 1000, 647, 0);
+  init_pair(TC_CELLI, COLOR_CYAN, COLOR_BLACK);
+  init_pair(TC_CELLJ, COLOR_BLUE, COLOR_BLACK);
+  init_pair(TC_CELLL, COLOR_WHITE, COLOR_BLACK);
+  init_pair(TC_CELLO, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(TC_CELLS, COLOR_GREEN, COLOR_BLACK);
+  init_pair(TC_CELLT, COLOR_MAGENTA, COLOR_BLACK);
+  init_pair(TC_CELLZ, COLOR_RED, COLOR_BLACK);
+}
+
+// Main tetris game!
+int main(int argc, char **argv) {
+  tetris_game *tg;
+  tetris_move move = TM_NONE;
+  bool running = true;
+  WINDOW *board, *next, *hold, *score;
+#if WITH_SDL
+  Mix_Music *music;
+#endif
+
+  // Load file if given a filename
+  if (argc >= 2) {
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+      perror("tetris");
+      exit(EXIT_FAILURE);
+    }
+    tg = tg_load(f);
+    fclose(f);
+  } else {
+    // Otherwise create new game
+    tg = tg_create(22, 10);
+  }
+
+#if WITH_SDL
+
+  // Initialize music
+  if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+    fprintf(stderr, "unable to intialize SDL\n");
+    exit(EXIT_FAILURE);
+  }
+  if (Mix_Init(MIX_INIT_MP3) != MIX_INIT_MP3) {
+    fprintf(stderr, "unable to intialize SDL_mixer\n");
+    exit(EXIT_FAILURE);
+  }
+  if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) != 0) {
+    fprintf(stderr, "unable to intialize audio\n");
+    exit(EXIT_FAILURE);
+  }
+  Mix_AllocateChannels(1); // only need background music
+  music = Mix_LoadMUS("tetris.mp3");
+  if (music) {
+    Mix_PlayMusic(music, -1);
+  }
+
+#endif
+  // NCURSES initialization:
+  initscr();            // initialize curses
+  cbreak();             // pass key presses to program, but not signals
+  noecho();             // don't echo key presses to screen
+  keypad(stdscr, TRUE); // initialize curses
+  timeout(0);           // initialize curses
+  curs_set(0);          // initialize curses
+  init_colors();        // initialize curses
+
+  // Create windows for each section of the interface
+  board = newwin(tg->rows + 2, 2 * tg->cols + 2, 0, 0);
+  next = newwin(6, 10, 0, 2 * (tg->cols + 1) + 1);
+  hold = newwin(6, 10, 7, 2 * (tg->cols + 1) + 1);
+  score = newwin(6, 10, 14, 2 * (tg->cols + 1) + 1);
+
+  // Game loop
+  while (running) {
+    running = tg_tick(tg, move);
+    display_board(board, tg);
+    display_piece(next, tg->next);
+    display_piece(hold, tg->stored);
+    display_score(score, tg);
+    doupdate();
+    sleep_milli(10);
+
+    switch (getch()) {
+    case KEY_LEFT:
+      move = TM_LEFT;
+      break;
+    case KEY_RIGHT:
+      move = TM_RIGHT;
+      break;
+    case KEY_UP:
+      move = TM_CLOCK;
+      break;
+    case KEY_DOWN:
+      move = TM_DROP;
+      break;
+    case 'q':
+      running = false;
+      move = TM_NONE;
+      break;
+    case 'p':
+      wclear(board);
+      box(board, 0, 0);
+      wmove(board, tg->rows / 2, (tg->cols * COLS_PER_CELL - 6) / 2);
+      wprintw(board, "PAUSED");
+      wrefresh(board);
+      timeout(-1);
+      getch();
+      timeout(0);
+      move = TM_NONE;
+      break;
+    case 'b':
+      boss_mode();
+      move = TM_NONE;
+      break;
+    case 's':
+      save(tg, board);
+      move = TM_NONE;
+      break;
+    case ' ':
+      move = TM_HOLD;
+      break;
+    default:
+      move = TM_NONE;
+    }
+  }
+
+  // Deinitialize NCurses
+  wclear(stdscr);
+  endwin();
+
+#if WITH_SDL
+
+  // Deinitialize Sound
+  Mix_HaltMusic();
+  Mix_FreeMusic(music);
+  Mix_CloseAudio();
+  Mix_Quit();
+
+#endif
+
+  // Output ending message
+  printf("Game over!\n");
+  printf("You finished with %d points on level %d. \n", tg->points, tg->level);
+
+  // Deinitialize Tetris
+  tg_delete(tg);
+  return 0;
+}
